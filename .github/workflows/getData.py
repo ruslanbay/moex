dcf77ce5cfa0587328d020eca39d275a45f9dfc1
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2023-07-31 19:25:10 +0300

    add action

--- .github/workflows/getData.py
+++ .github/workflows/getData.py
@@ -3,16 +3,22 @@ import requests
 import sys
 
 def daterange(start, end, stepDays):
-    for n in range(0, int((end - start).days), stepDays):
-        yield start + timedelta(n)
+  for n in range(0, int((end - start).days), stepDays):
+    yield start + timedelta(n)
 
-# start = date(2011, 12, 19) # Monday
-start = datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
-end = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
-step = int(sys.argv[3])
-
-for d in daterange(start, end, step):
-  with open(f'data/iss/history/engines/stock/totals/boards/MRKT/securities-{d}.json', 'w', encoding='utf-8') as f:
-    r = requests.get(f'https://iss.moex.com/iss/history/engines/stock/totals/boards/MRKT/securities.json?iss.meta=off&date={d}&securities.columns=SECID,CURRENCYID,OPEN,CLOSE,VOLUME,VALUE,NUMTRADES,DAILYCAPITALIZATION')
+def getData(myDate):
+  # data/iss/history/engines/stock/totals/boards/MRKT/securities-{myDate}.json
+  with open(f'tmp_data/securities-{myDate}.json', 'w', encoding='utf-8') as f:
+    r = requests.get(f'https://iss.moex.com/iss/history/engines/stock/totals/boards/MRKT/securities.json?iss.meta=off&date={myDate}&securities.columns=SECID,CURRENCYID,OPEN,CLOSE,VOLUME,VALUE,NUMTRADES,DAILYCAPITALIZATION')
     f.write(r.text)
     f.close()
+
+# start = date(2011, 12, 19) # Monday
+if(len(sys.argv) == 4):
+  start = datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
+  end = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
+  step = int(sys.argv[3])
+  for myDate in daterange(start, end, step):
+    getData(myDate)
+else:
+  getData(date.today() - timedelta(days=1))
