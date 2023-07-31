207d250f9ee419c112e76d951949e22242be555b
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2023-07-31 16:38:36 +0300

    fix

--- .github/workflows/getData.py
+++ .github/workflows/getData.py
@@ -1,15 +1,15 @@
 from datetime import date, timedelta
 import requests
 
-def daterange(start, end):
-    for n in range(int((end - start).days)):
-        yield start + timedelta(n + 7)
+def daterange(start, end, stepDays):
+    for n in range(0, int((end - start).days), stepDays):
+        yield start + timedelta(n)
 
-start = date(2011, 12, 19) # Monday
-# start = date(2023, 7, 3)
+# start = date(2011, 12, 19) # Monday
+start = date(2023, 7, 3)
 end = date(2023, 7, 28)
       
-for d in daterange(start, end):
+for d in daterange(start, end, 7):
   # print(d.strftime("%Y-%m-%d"))
   with open(f'data/iss/history/engines/stock/totals/boards/MRKT/securities-{d}.json', 'w', encoding='utf-8') as f:
     r = requests.get(f'https://iss.moex.com/iss/history/engines/stock/totals/boards/MRKT/securities.json?iss.meta=off&date={d}&securities.columns=SECID,CURRENCYID,OPEN,CLOSE,VOLUME,VALUE,NUMTRADES,DAILYCAPITALIZATION')
