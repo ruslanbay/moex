ebb134fbe2d2622da38a3e617eb0e68e3639f22d
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2023-07-31 17:01:02 +0300

    minor fix

--- .github/workflows/getData.py
+++ .github/workflows/getData.py
@@ -7,10 +7,11 @@ def daterange(start, end, stepDays):
         yield start + timedelta(n)
 
 # start = date(2011, 12, 19) # Monday
-# start = date(2023, 7, 3)
-# end = date(2023, 7, 28)
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+step = int(sys.argv[3])
       
-for d in daterange(sys.argv[1], sys.argv[2], sys.argv[3]):
+for d in daterange(start, end, step):
   with open(f'securities-{d}.json', 'w', encoding='utf-8') as f:
     r = requests.get(f'https://iss.moex.com/iss/history/engines/stock/totals/boards/MRKT/securities.json?iss.meta=off&date={d}&securities.columns=SECID,CURRENCYID,OPEN,CLOSE,VOLUME,VALUE,NUMTRADES,DAILYCAPITALIZATION')
     f.write(r.text)
