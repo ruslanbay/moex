a42c2973f4cc260353c1e3e5993f32c8a9daf7c6
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2023-08-09 15:57:56 +0300

    Update processLocalData.py

--- .github/workflows/processLocalData.py
+++ .github/workflows/processLocalData.py
@@ -46,12 +46,11 @@ if(len(sys.argv) == 4):
       # If the current ticket didn't appear before,
       # then set capitalization values for previuose dates as 0.00
       if(ticket not in chartData.keys()):
-        chartData[ticket] = {'x': [f'{start}'], 'y': [{cap}]}
+        chartData[ticket] = {'x': [f'{start}'], 'y': [cap]}
         for d in daterange(start + timedelta(days=step), myDate - timedelta(days=step), step):
           chartData[ticket]["x"].append(f'{d}')
           chartData[ticket]["y"].append(0.00)
-      # if(ticket in chartData.keys()):
-      else:
+      else: # if(ticket in chartData.keys()):
         lastDate = chartData[ticket]["x"][-1]
         lastDate = datetime.strptime(lastDate, '%Y-%m-%d').date()
         # if there are skips in dates, then fill capitalization values with 0.00
