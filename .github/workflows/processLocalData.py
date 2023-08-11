78bb791df74012dbf340eb0ac3ceb691040b6266
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2023-08-11 18:05:37 +0300

    fill tonext

--- .github/workflows/processLocalData.py
+++ .github/workflows/processLocalData.py
@@ -50,7 +50,7 @@ if(len(sys.argv) == 4):
         traces[ticket] = {
           'name': ticket,
           'type': 'scatter',
-          'fill': 'tonextx',
+          'fill': 'tonext',
           'stackgroup': 'one',
           'hoverinfo': 'skip',
           'hovertemplate': '',
