11bc9857065e7b51c8757f3ae7487f6eeb647f18
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2024-09-15 13:11:18 +0000

    Update updateTicketList.py

--- .github/workflows/updateTicketList.py
+++ .github/workflows/updateTicketList.py
@@ -2,36 +2,45 @@ import pandas as pd
 import requests
 import json
 
-# Load the TSV file into a DataFrame
 df = pd.read_csv('data/issues-by-sector.tsv', sep='\t', skiprows=range(1, 16), header=0)
 
-# Function to fetch the history_from date for a given label
-def fetch_history_from(label):
-    url = f"https://iss.moex.com/iss/securities/{label}.json?iss.meta=off"
-    response = requests.get(url)
+def getCompanyInfo(ticker):
+  url = f"https://iss.moex.com/iss/securities/{ticker}.json?iss.meta=off"
+  response = requests.get(url)
+  
+  if response.status_code == 200:
+    data = response.json()
     
-    if response.status_code == 200:
-        data = response.json()
+    with open(f"data/iss/securities/{ticker}.json", 'w', encoding='utf-8') as json_file:
+      json.dump(data, json_file, ensure_ascii=False, indent=4)
+    
+    try:
+      companyInfo = dict()
+      for item in data['description']['data']:
+        match item[0]:
+          case 'NAME':
+            companyInfo['NAME'] = item[2]
+          case 'SHORTNAME':
+            companyInfo['SHORTNAME'] = item[2]
+          case 'LATNAME':
+            companyInfo['LATNAME'] = item[2]
+          case 'ISSUEDATE':
+            companyInfo['ISSUEDATE'] = item[2]
+          case _:
+            continue
+      return companyInfo
+    except (IndexError, KeyError):
+      return None
+  else:
+    print(f"Error fetching data for {ticker}: {response.status_code}")
+    return None
 
-        # Save the response as a JSON file
-        with open(f"data/iss/securities/{label}.json", 'w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
-        
-        # Extract the ISSUEDATE from the description.data
-        try:
-            issuedate = data['description']['data'][8][2]
-            return issuedate
-        except (IndexError, KeyError):
-            return None
-    else:
-        print(f"Error fetching data for {label}: {response.status_code}")
-        return None
-
-# Update the history_from column for each label
 for index, row in df.iterrows():
-    label = row['labels']
-    history_from = fetch_history_from(label)
-    df.at[index, 'history_from'] = history_from
+  ticker = row['labels']
+  companyInfo = getCompanyInfo(ticker)
+  df.at[index, 'name_rus'] = companyInfo['NAME']
+  df.at[index, 'shortname_rus'] = companyInfo['SHORTNAME']
+  df.at[index, 'shortname'] = companyInfo['LATNAME']
+  df.at[index, 'history_from'] = companyInfo['ISSUEDATE']
 
-# Save the updated DataFrame back to a TSV file
 df.to_csv('data/updated_issues-by-sector.tsv', sep='\t', index=False)
